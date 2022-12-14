12410f1f420a29b017c89cb15711e6a798a26ac3
Szymon Nagel <szmnnagel@gmail.com>
2022-12-14 17:37:54 +0100

    Fixed Size and API key

--- data_downloader.py
+++ data_downloader.py
@@ -8,10 +8,13 @@ import tqdm
 from pexels_api import API
 
 MAX_IMAGES = 1200
-RESULTS_PER_PAGE = 100
+RESULTS_PER_PAGE = 80
 PAGE_LIMIT = MAX_IMAGES / RESULTS_PER_PAGE
 
-PEXELS_API_KEY = '563492ad6f917000010000016a8b628f9c414303b78470894a6168dd'
+
+with open('Config.json') as settings:
+    PEXELS_API_KEY = json.load(settings)["API_KEY"]
+
 api = API(PEXELS_API_KEY)
 photos_dict = {}
 page = 1
@@ -22,6 +25,9 @@ parser.add_argument('query')
 args = parser.parse_args()
 query = args.query
 
+
+
+print(PEXELS_API_KEY)
 # Step 1: Getting urls and meta information
 while page < PAGE_LIMIT:
     api.search(query, page=page, results_per_page=RESULTS_PER_PAGE)
@@ -31,28 +37,27 @@ while page < PAGE_LIMIT:
         counter += 1
         if not api.has_next_page:
             break
-        page += 1
+    page += 1
 
 print(f"Finishing at page: {page}")
 print(f"Images were processed: {counter}")
 
 # Step 2: Downloading
-PATH = f'./alcohol/{query}/'
+PATH = f'./data/alcohol/{query}/'
 RESOLUTION = 'original'
+SIZE = 512
+
 
 if photos_dict:
     os.makedirs(PATH, exist_ok=True)
-
-    # Saving dict
-    with open(os.path.join(PATH, f'{query}.json'), 'w') as fout:
-        json.dump(photos_dict, fout)
-
+    
     for val in tqdm.tqdm(photos_dict.values()):
         url = val['src'][RESOLUTION]
         fname = os.path.basename(val['src']['original'])
+        download = url+f"?auto=compress&cs=tinysrgb&h={SIZE}&w={SIZE}"
         image_path = os.path.join(PATH, fname)
         if not os.path.isfile(image_path):
-            response = requests.get(url, stream=True)
+            response = requests.get(download, stream=True)
             with open(image_path, 'wb') as outfile:
                 outfile.write(response.content)
         else:
